f537f2ceec10e0977287e70a1add01a9d5bac0a1
Dennis Kleinhans <dennis.kleinhans991@gmx.de>
2024-02-27 14:19:51 +0100

    add class JobModel for the database

--- scraper/data_objects.py
+++ scraper/data_objects.py
@@ -1,3 +1,9 @@
+from sqlalchemy import ARRAY, Boolean, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
 class Job:
     def __init__(
         self,
@@ -12,3 +18,14 @@ class Job:
         self.locations = locations
         self.home_office = home_office
         self.badges = badges
+
+
+class JobModel(Base):
+    __tablename__ = "jobs"
+
+    id = Column(String, primary_key=True)
+    company = Column(String)
+    description = Column(String)
+    locations = Column(ARRAY(String))
+    home_office = Column(Boolean)
+    badges = Column(ARRAY(String))
