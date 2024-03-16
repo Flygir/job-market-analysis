eaf9fd6cd4fc1c676338c39942ca2c8232e5c3ff
Dennis Kleinhans <dennis.kleinhans991@gmx.de>
2024-03-16 18:14:40 +0100

    change folder name to solve naming conflicts

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -1,7 +1,6 @@
 from scraper import GetInItScraper
 from handler import DatabaseHandler
 
-
 connection_string = "postgresql://job-analysis:1234@localhost:5432/job-analysis"
 scraper = GetInItScraper(DatabaseHandler(connection_string))
-scraper.scrape_jobs()
+scraper.scrape_jobs_parallel()
